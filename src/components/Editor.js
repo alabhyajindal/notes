b6c948aef2147d0acf092191980f598cf6da2bb8
Alabhya Jindal <alabhya10@gmail.com>
2022-03-31 22:28:04 +0530

    changed input to textarea for editor-body

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 function Editor(props) {
+  const body = document.querySelector(".editor-body");
+  // console.log(body.value);
+
   return (
     <div className="editor-cont">
       <input
@@ -9,12 +12,15 @@ function Editor(props) {
         value={props.currentNote().title}
         name="title"
       />
-      <input
-        className="editor-body"
-        onChange={props.updateNote}
-        value={props.currentNote().body}
-        name="body"
-      />
+      <div className="editor-body-cont">
+        <textarea
+          className="editor-body"
+          onChange={props.updateNote}
+          value={props.currentNote().body}
+          name="body"
+        ></textarea>
+        {/* <p className="editor-b">{body && body.value}</p> */}
+      </div>
     </div>
   );
 }
