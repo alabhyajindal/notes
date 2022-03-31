83495d8d6e85387159052bb7636a8f336783de0d
Alabhya Jindal <alabhya10@gmail.com>
2022-03-31 21:08:09 +0530

    increased width of editor-body

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Editor from "./components/Editor";
 import Sidebar from "./components/Sidebar";
 import "./style.css";
