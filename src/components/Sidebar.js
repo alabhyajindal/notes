997719022bc386a83591ee1e439ebf203f5d0c3f
Alabhya Jindal <alabhya10@gmail.com>
2022-04-08 09:41:31 +0530

    updated meta description

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -18,15 +18,19 @@ function Sidebar(props) {
         </button>
       </header>
       <div className="section-heading-cont">
-        <div className="section-heading">
+        {/* <div className="section-heading">
           <CollectionIcon className="section-heading-icon" />
           <h2 className="all-notes">All Notes</h2>
-        </div>
+        </div> */}
+        {/* <div className="section-heading">
+            <GiftIcon className="section-heading-icon" />
+            <Link to="/favorites" className="favorites">
+              Favorites
+            </Link>
+          </div> */}
         <div className="section-heading">
           <GiftIcon className="section-heading-icon" />
-          <Link to="/favorites" className="favorites">
-            Favorites
-          </Link>
+          <h2 className="favorites">Favorites</h2>
         </div>
       </div>
       {/* <Outlet /> */}
